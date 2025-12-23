65b947f66d5c4c4687150b7e38405d369143d9e8
Moha Sahi <16020356+MAWAAW@users.noreply.github.com>
2025-12-23 15:57:37 +0100

    Refactor Block class and improve encapsulation
    Refactored Block class to use private access modifiers for hash and previousHash. Removed duplicate constructor and calculateHash method, and updated mining output message.

--- Block.java
+++ Block.java
@@ -4,40 +4,42 @@ import java.util.Date;
 
 public class Block {
 	
-	public String hash;
-	public String previousHash; 
+	private String hash;
+	private String previousHash; 
 	private String data; //our data will be a simple message.
 	private long timeStamp; //as number of milliseconds since 1/1/1970.
 	private int nonce;
-	
-	//Block Constructor.  
-	public Block(String data,String previousHash ) {
-		this.data = data;
-		this.previousHash = previousHash;
-		this.timeStamp = new Date().getTime();
-		
-		this.hash = calculateHash(); //Making sure we do this after we set the other values.
-	}
-	
-	//Calculate new hash based on blocks contents
-	public String calculateHash() {
-		String calculatedhash = StringUtil.applySha256( 
-				previousHash +
-				Long.toString(timeStamp) +
-				Integer.toString(nonce) + 
-				data 
-				);
-		return calculatedhash;
-	}
+
+	public Block(String data, String previousHash) {
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+        this.hash = calculateHash();
+    }
+
+    public String calculateHash() {
+        return StringUtil.applySha256(
+                previousHash +
+                        timeStamp +
+                        nonce +
+                        data
+        );
+    }
 	
 	//Increases nonce value until hash target is reached.
 	public void mineBlock(int difficulty) {
 		String target = StringUtil.getDificultyString(difficulty); //Create a string with difficulty * "0" 
 		while(!hash.substring( 0, difficulty).equals(target)) {
-			nonce ++;
+			nonce++;
 			hash = calculateHash();
 		}
-		System.out.println("Block Mined!!! : " + hash);
+		System.out.println("✅ Block Mined: " + hash);
 	}
-	
+
+    // Getters
+    public String getHash() { return hash; }
+    public String getPreviousHash() { return previousHash; }
+    public String getData() { return data; }
+    public long getTimeStamp() { return timeStamp; }
+    public int getNonce() { return nonce; }
 }
